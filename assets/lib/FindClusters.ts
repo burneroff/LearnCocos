e15b9270d72d5e266751196960725adf293c4064
Nikita Karaban <net.cheteram@gmail.com>
2025-09-25 18:01:57 +0300

    init project

---
+++ assets/lib/FindClusters.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('FindClusters')
+export class FindClusters extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
