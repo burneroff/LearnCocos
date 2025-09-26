b46e150c9ee7cb5f412c94680ebbb6698168b181
Nikita Karaban <net.cheteram@gmail.com>
2025-09-26 15:42:28 +0300

    v1.1 ready

--- assets/lib/GameManager.ts
+++ assets/lib/GameManager.ts
@@ -15,7 +15,7 @@ interface Field {
   color: string;
   x: number;
   y: number;
-  node?: Node; // ссылка на префаб, чтобы включать/выключать Label
+  node?: Node; 
 }
 
 @ccclass("GameMenager")
@@ -35,20 +35,12 @@ export class GameMenager extends Component {
   @property(EditBox)
   inputY: EditBox = null!;
 
-  @property
-  fieldWidthM = 5;
-
-  @property
-  fieldLengthN = 5;
-
-  @property
-  fieldColorsX = 0;
-
-  @property
-  fieldMinClasterSizeY = 0;
+  private fieldWidthM = 5;
+  private fieldLengthN = 5;
+  private fieldColorsX = 0;
+  private fieldMinClasterSizeY = 0;
 
   private _field: Field[][] = [];
-  private _gameStarted = false;
   private _visited: boolean[][] = [];
   private _colorArr = [
     "#FF0000",
@@ -59,82 +51,8 @@ export class GameMenager extends Component {
     "#FF00FF",
     "#FFA500",
     "#800080",
+    "#FFFFFF",
   ];
-
-  start() {
-    this.inputM.node.on("editing-did-ended", this.onInputChange, this);
-    this.inputN.node.on("editing-did-ended", this.onInputChange, this);
-    this.inputX.node.on("editing-did-ended", this.onInputChange, this);
-    this.inputY.node.on("editing-did-ended", this.onInputChange, this);
-  }
-
-  onInputChange() {
-    this.fieldWidthM = parseInt(this.inputM.string) || this.fieldWidthM;
-    this.fieldLengthN = parseInt(this.inputN.string) || this.fieldLengthN;
-    this.fieldColorsX = parseInt(this.inputX.string) || this.fieldColorsX;
-    this.fieldMinClasterSizeY =
-      parseInt(this.inputY.string) || this.fieldMinClasterSizeY;
-
-    console.log(
-      "Новые параметры:",
-      this.fieldWidthM,
-      this.fieldLengthN,
-      this.fieldColorsX,
-      this.fieldMinClasterSizeY
-    );
-  }
-
-  generateField() {
-    this._field = [];
-
-    for (let i = 0; i < this.fieldWidthM; i++) {
-      let row: Field[] = [];
-      for (let j = 0; j < this.fieldLengthN; j++) {
-        row.push({
-          color:
-            this._colorArr[Math.floor(Math.random() * this._colorArr.length)],
-          x: 50 * j,
-          y: 50 * i,
-        });
-      }
-      this._field.push(row);
-    }
-
-    console.log("Field generated");
-    this.spawnBlocks();
-  }
-
-  spawnBlocks() {
-    if (!this.curPrefab) {
-      console.warn("Prefab is not assigned!");
-      return;
-    }
-
-    for (let i = 0; i < this.fieldWidthM; i++) {
-      for (let j = 0; j < this.fieldLengthN; j++) {
-        let block = instantiate(this.curPrefab);
-        this.node.addChild(block);
-        block.setPosition(this._field[i][j].x, this._field[i][j].y);
-
-        // Запоминаем узел внутри field
-        this._field[i][j].node = block;
-
-        // Задаём цвет спрайта
-        let sprite = block.getComponentInChildren(Sprite);
-        if (sprite) {
-          sprite.color = Color.fromHEX(new Color(), this._field[i][j].color);
-        }
-
-        // Выключаем Label по умолчанию
-        let label = block.getComponentInChildren(Label);
-        if (label) {
-          label.enabled = false;
-        }
-      }
-    }
-
-    console.log("Blocks spawned");
-  }
 
   // DFS обход
   private dfs(i: number, j: number, color: string, cluster: Field[]) {
@@ -159,6 +77,57 @@ export class GameMenager extends Component {
     this.dfs(i, j - 1, color, cluster);
   }
 
+  generateField() {
+    this._field = [];
+
+    for (let i = 0; i < this.fieldWidthM; i++) {
+      let row: Field[] = [];
+      for (let j = 0; j < this.fieldLengthN; j++) {
+        row.push({
+          color: this._colorArr[Math.floor(Math.random() * this.fieldColorsX)],
+          x: 50 * j,
+          y: 50 * i,
+        });
+      }
+      this._field.push(row);
+    }
+
+    console.log("Field generated");
+  }
+
+  spawnBlocks() {
+    if (!this.curPrefab) {
+      console.warn("Prefab is not assigned!");
+      return;
+    }
+
+    for (let i = 0; i < this.fieldWidthM; i++) {
+      for (let j = 0; j < this.fieldLengthN; j++) {
+        let block = instantiate(this.curPrefab);
+        this.node.addChild(block);
+        block.setPosition(this._field[i][j].x, this._field[i][j].y);
+
+        // Запоминаем узел внутри field
+        this._field[i][j].node = block;
+
+        // Задаём цвет спрайта
+        let sprite = block.getComponentInChildren(Sprite);
+
+        if (sprite) {
+          sprite.color = Color.fromHEX(new Color(), this._field[i][j].color);
+        }
+
+        // Выключаем Label по умолчанию
+        let label = block.getComponentInChildren(Label);
+        if (label) {
+          label.enabled = false;
+        }
+      }
+    }
+
+    console.log("Blocks spawned");
+  }
+
   findClusters() {
     this._visited = Array.from({ length: this.fieldWidthM }, () =>
       Array(this.fieldLengthN).fill(false)
@@ -172,8 +141,6 @@ export class GameMenager extends Component {
 
           // если кластер достаточно большой — включаем Label у всех блоков
           if (cluster.length >= this.fieldMinClasterSizeY) {
-            console.log(cluster.length, ": length");
-            console.log(this.fieldMinClasterSizeY, ": length2");
             for (let cell of cluster) {
               let label = cell.node?.getComponentInChildren(Label);
               if (label) {
@@ -186,14 +153,23 @@ export class GameMenager extends Component {
     }
   }
 
+  onInputChange() {
+    this.fieldWidthM = parseInt(this.inputM.string) || this.fieldWidthM;
+    this.fieldLengthN = parseInt(this.inputN.string) || this.fieldLengthN;
+    this.fieldColorsX = parseInt(this.inputX.string) || this.fieldColorsX;
+    this.fieldMinClasterSizeY =
+      parseInt(this.inputY.string) || this.fieldMinClasterSizeY;
+  }
+
   onStartButtonClicked() {
     this.node.removeAllChildren();
     this._field = [];
 
-    // 2. Сгенерировать новое
     this.generateField();
+    this.spawnBlocks();
     this.findClusters();
   }
 
+  start() {}
   update(deltaTime: number) {}
 }
